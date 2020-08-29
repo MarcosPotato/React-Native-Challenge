916573abb4b328becb6e4861cfba5f912980c895
Marcos Moreira <marquinhos2725@hotmail.com>
2020-08-29 19:40:40 -0300

    Adding a resolution of Challenge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useState, useEffect} from 'react'
 import {
   SafeAreaView,
   View,
@@ -10,46 +10,64 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import api from './services/api.js'
+
 export default function App() {
+  const [repositories, setRepositories] = useState([])
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    try{
+      const response = await api.post(`/repositories/${id}/like`)
+      if(response.status === 200){
+        const updatedData = repositories.map(repository => (repository.id === id) ? response.data : repository)
+        setRepositories(updatedData)
+      }
+    } catch(err){
+      console.log(err)
+    }
   }
+
+  useEffect(() => {
+    api.get('/repositories')
+      .then(response => setRepositories(response.data))
+      .catch(err => console.log(err))
+  },[])
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
-
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+      <FlatList
+          data={ repositories }
+          keyExtractor={ repository => repository.id.toString() }
+          renderItem={({ item: repository }) => (
+            <View style={styles.repositoryContainer}>
+              <Text style={styles.repository}>{repository.title}</Text>
+              <View style={styles.techsContainer}>
+                {
+                  repository.techs.map(tech => (
+                    <Text key={tech} style={styles.tech}>{tech}</Text>
+                  ))
+                }
+              </View>
+              <View style={styles.likesContainer}>
+                <Text
+                  style={styles.likeText}
+                  testID={`repository-likes-${repository.id}`}
+                >
+                  {`${repository.likes} curtidas`}
+                </Text>
+              </View>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleLikeRepository(repository.id)}
+                testID={`like-button-${repository.id}`}
+              >
+                <Text style={styles.buttonText}>Curtir</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        />
       </SafeAreaView>
     </>
   );
@@ -61,6 +79,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#7159c1",
   },
   repositoryContainer: {
+    borderRadius: 10,
     marginBottom: 15,
     marginHorizontal: 15,
     backgroundColor: "#fff",
@@ -71,6 +90,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   techsContainer: {
+    borderRadius: 5,
     flexDirection: "row",
     marginTop: 10,
   },
@@ -95,6 +115,7 @@ const styles = StyleSheet.create({
   },
   button: {
     marginTop: 10,
+    borderRadius: 10,
   },
   buttonText: {
     fontSize: 14,
